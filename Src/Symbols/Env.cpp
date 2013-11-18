8125cfe4c4bb61dc08c008fe7d1ac4f5815df137
Ben Zerhsuen <zerhusbt@mail.uc.edu>
2013-11-18 17:59:52 -0500

    Updated files for seting up symbol table environments

--- Src/Symbols/Env.cpp
+++ Src/Symbols/Env.cpp
@@ -1,13 +1,14 @@
 //*****************************************************************************************
 //
 //	Filename: Env.cpp
-//	Description: File for ???
+//	Description: Header file for setting up the environments for symbol tables (chained
+//		symbol tables to account for scopes)
 //	Author: Benjamin Zerhusen
 //	Date Created: 11/16/2013
-//	Last Modified: 11/16/2013
+//	Last Modified: 11/18/2013
 //
 //*****************************************************************************************
 
 #include "Env.h"
 
-
+Env* Env::prev = new Env;
--- Src/Symbols/Env.h
+++ Src/Symbols/Env.h
@@ -1,10 +1,31 @@
 //*****************************************************************************************
 //
 //	Filename: Env.h
-//	Description: File for ???
+//	Description: File for setting up the environments for symbol tables (chained
+//		symbol tables to account for scopes)
 //	Author: Benjamin Zerhusen
 //	Date Created: 11/16/2013
-//	Last Modified: 11/16/2013
+//	Last Modified: 11/18/2013
 //
 //*****************************************************************************************
 
+#ifndef ENV_H
+#define ENV_H
+
+#include <map>
+
+class Env
+{
+	public:
+		void put(Token w,Id i);
+		Id get(Token w);
+		// Constructor
+		Env(Env n);
+	protected:
+		static Env* prev;
+	private:
+		//creation of hashtable (map)
+		std::map<std::string,Word> table;
+};
+
+#endif
