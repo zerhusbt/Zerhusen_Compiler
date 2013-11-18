1c64b6bfc8c0f6aca5c3b0072d86fc5bb96a0dc4
Ben Zerhusen <zerhusbt@mail.uc.edu>
2013-11-17 20:35:03 -0500

    First Two Files

---
+++ Src/Lexer/main.cpp
@@ -0,0 +1,24 @@
+//*****************************************************************************************
+//
+//	Filename: main.cpp
+//	Description: File called to execute compiler
+//	Author: Benjamin Zerhusen
+//	Date Created: 8/20/2013
+//	Last Modified: 11/11/2013
+//
+//*****************************************************************************************
+
+#include "main.h"
+#include <typeinfo>
+
+int main ()
+{
+	Lexer* myLexer = new Lexer();
+	Token* pMyToken = myLexer->scan();
+	cout<<pMyToken->toString()<<endl;
+	cout<<pMyToken->tokenType()<<endl;
+	pMyToken = myLexer->scan();
+	cout<<pMyToken->toString()<<endl;
+	cout<<pMyToken->tokenType()<<endl;
+	return 0;
+}
---
+++ Src/Lexer/main.h
@@ -0,0 +1,18 @@
+//*****************************************************************************************
+//
+//	Filename: main.h
+//	Description: Header file to include header files from other modules
+//	Author: Benjamin Zerhusen
+//	Date Created: 8/20/2013
+//	Last Modified: 11/11/2013
+//
+//*****************************************************************************************
+
+#include <iostream>
+#include "Tag.h"
+#include "Token.h"
+#include "Num.h"
+#include "Word.h"
+#include "Lexer.h"
+
+using namespace std;
