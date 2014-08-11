9a838c3335f32377be9fbce44621d50e586014c9
Ben Zerhusen <zerhusbt@mail.uc.edu>
2014-08-10 23:00:52 -0400

    Adding file handling class

---
+++ Src/Main/Files.h
@@ -0,0 +1,26 @@
+//*****************************************************************************************
+//
+//	Filename: FileHandler.h
+//	Description: Header file for ???
+//	Author: Benjamin Zerhusen
+//	Date Created: 11/22/2013
+//	Last Modified: 11/22/2013
+//
+//*****************************************************************************************
+
+#ifndef FILES_H
+#define FILES_H
+
+#include <iostream>
+
+class Files
+{
+	public:
+		static FILE* pInputFile;
+		static FILE* pOutputFile;
+		// constructor
+		Files();
+
+};
+
+#endif
